ef319d6706ace5b17007e7a411ca5cd750ca6bf3
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-01 13:30:55 +0000

    oki

--- get_data_from_urls.py
+++ get_data_from_urls.py
@@ -10,6 +10,7 @@ urlsSplit =  urls.split(" ") # each url is split by whatever character is in her
 contentTitles = []
 contentPages = []
 contentCombined = ""
+contentObj = {"intents": {}}
 
 for url in urlsSplit:
     response = requests.get('http://', url) #Http may better 
@@ -20,6 +21,7 @@ for url in urlsSplit:
     contentTitles.appeend(title)
     contentPages.append(summary)
     contentCombined += (title + " " + summary + ",\n")
+    contentObj[]
 
 def printresults():
     print(contentTitles)
