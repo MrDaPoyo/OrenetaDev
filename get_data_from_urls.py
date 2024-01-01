779c8a7b2ec1a323a2b2035ba90bc049f01ccb42
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-01 13:12:32 +0000

    Yay!

--- get_data_from_urls.py
+++ get_data_from_urls.py
@@ -1,12 +1,21 @@
 # doing it here, bc then we have it for another time if needed
-# great!
 import requests
 from readability import Document
 
+df = open("tools/url-list.txt") 
 
+urls = df.read()
+urlsSplit =  urls.split("\n")
+
+contentTitles = []
+contentPages = []
 
 for url in urls:
-    response = requests.get('http://example.com')
+    response = requests.get('http://'+url) #Http may better 
+    #considering Ucanet is mostly used by Windows 95 computers that ain't using Https
     doc = Document(response.content)
-    doc.title()
-    doc.summary()
+    contentTitles.appeend(doc.title())
+    contentPages.append(doc.summary())
+
+print(contentTitles)
+print(contentPages)
