b6a23d274fc2dbaaa656eca6045ea65e5a465e9c
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-01 13:18:54 +0000

    Some progress here

--- get_data_from_urls.py
+++ get_data_from_urls.py
@@ -4,19 +4,24 @@ from readability import Document
 ''' df = open("tools/url-list.txt") 
 
 urls = df.read()'''
-urlsSplit =  urls.split("\n")
+urls = "wikipedia.com neal.fun dapoyo.neocities.org theoldnet.com 12thman.com"
+urlsSplit =  urls.split(" ") # each url is split by whatever character is in here
 
 contentTitles = []
 contentPages = []
 contentCombined = ""
 
-for url in urls:
-    response = requests.get('http://'+url) #Http may better 
+for url in urlsSplit:
+    response = requests.get('http://', url) #Http may better 
     #considering Ucanet is mostly used by Windows 95 computers that ain't using Https
     doc = Document(response.content)
-    contentTitles.appeend(doc.title())
-    contentPages.append(doc.summary())
-    contentCombined += 
+    title = doc.title()
+    summary = doc.summary
+    contentTitles.appeend(title)
+    contentPages.append(summary)
+    contentCombined += (title + " " + summary + ",\n")
 
 print(contentTitles)
 print(contentPages)
+print("--------Results--------")
+print(contentCombined)
