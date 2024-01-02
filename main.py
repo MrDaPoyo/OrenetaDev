499218bf197093536fbd465742c7e7a690a5065d
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-02 14:26:09 +0000

    Render_template

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify, render_template # <-
+
+app = Flask(__name__)
+
+@app.route("/get-user<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+    }
+
+    return render_template('frontend/load_url.html', user_data=jsonify(user_data))
+    # return jsonify(user_data), 200
+    # render_template isn't defined; not anymore oh it seems to work now
+
+if __name__ == "__main__":
+    app.run(debug=True)
