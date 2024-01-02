300a0015ecae550a7bb950c43975c3d70ba99b99
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-02 14:11:28 +0000

    Commenting
    Co-authored-by: Silver <SilverSpace505@users.noreply.github.com>

--- AI/main..py
+++ AI/main..py
@@ -5,20 +5,27 @@ import numpy as np
 import nltk 
 from keras.models import load_model 
 from nltk.stem import WordNetLemmatizer 
-from flask import Flask, request, jsonify 
-  
+from flask import Flask, request, jsonify, render_template
+ 
+# Importing all the libraries we do need ^
+# Loading and setting up stuff 
+
 lemmatizer = WordNetLemmatizer() 
 intents = json.loads(open("AI/intents.json").read()) 
 words = pickle.load(open('AI/words.pkl', 'rb')) 
 classes = pickle.load(open('AI/classes.pkl', 'rb')) 
 model = load_model('AI/chatbotmodel.h5') 
   
+#cleans the sentence by removing connectors such as "like", "am", "are" and 
+# keeps keywords like "popular", "great" and "cool"
+
 def clean_up_sentences(sentence): 
     sentence_words = nltk.word_tokenize(sentence) 
     sentence_words = [lemmatizer.lemmatize(word) 
                       for word in sentence_words] 
     return sentence_words 
-  
+
+
 def bagw(sentence): 
     sentence_words = clean_up_sentences(sentence) 
     bag = [0]*len(words) 
@@ -51,7 +58,7 @@ def get_response(intents_list, intents_json):
             break
     return result 
   
-print("Chatbot is up!") 
+print("Chatbot is up!")
   
 ''' TODO: Replace this with the API 
 while True: 
@@ -60,7 +67,11 @@ while True:
     res = get_response(ints, intents) 
     print(res) '''
 
+#setting up the Flask server
+
 app = Flask(__name__)
+
+# If the user goes to /search-avocadoes, the server will use "avocadoes" as input
 
 @app.route("/search-<user_id>")
 def query(user_id):
@@ -70,5 +81,6 @@ def query(user_id):
     return jsonify(res), 200
 
 
+# Enable debug mode, change on production
 if __name__ == "__main__":
     app.run(debug=True)
