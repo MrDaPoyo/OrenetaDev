f383e63c1b7d9121f7d33b8177a91c2be9332b75
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-02 15:10:21 +0000

    Improved lots of code

--- get_data_from_urls.py
+++ get_data_from_urls.py
@@ -3,13 +3,14 @@ import requests
 from readability.readability import Document
 import json
 import re
+import time
 
 '''
 df = open("tools/url-list.txt") 
 urls = df.read()
 '''
 
-urls = "google.com youtube.com"
+urls = "google.com youtube.com facebook.com baidu.com wikipedia.org amazon.com qq.com reddit.com taobao.com yahoo.com twitter.com instagram.com imdb.com stackexchange.com nasa.gov espn.com nba.com weather.com nintendo.com steamcommunity.com xfinity.com walmart.com homedepot.com target.com chase.com bankofamerica.com Fargo.com stanford.edu columbia.edu berkeley.edu dapoyo.neocities.org accuweather.com pandora.com last.fm"
 urlsSplit =  urls.split(" ") # each url is split by whatever character is in here
 
 contentTitles = []
@@ -20,6 +21,7 @@ contentObj = {"intents": []}
 
 
 for url in urlsSplit:
+    start_time = time.time()
     response = requests.get('http://' + url) #Http may be better 
     #considering Ucanet is mostly used by Windows 95 computers that ain't using Https
     doc = Document(response.content)
@@ -33,6 +35,10 @@ for url in urlsSplit:
         "keywords": (title + " " + summary).split(" "), 
         "response": ["http://"+url]
     })
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(url + " done in " + str(elapsed_time))
+    time.sleep(0)
 
 def printresults():
     print(contentTitles)
@@ -41,11 +47,12 @@ def printresults():
     print(contentCombined)
     formattedcontent = remove_html_tags(str(contentObj))
     print(formattedcontent) # I'm gonna need this converted to JSON text
-    # print(json.dump(contentObj))
+    with open('intents.json', 'w', encoding ='utf8') as json_file: 
+        json.dump(formattedcontent, json_file, ensure_ascii = False)
 
 def remove_html_tags(text):
     """Remove html tags from a string"""
-    clean = re.compile('<.*? \n>')
+    clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
 
 printresults()
