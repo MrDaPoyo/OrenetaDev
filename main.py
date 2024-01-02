c68670e7ca2fd2752191b31c57d22d20d8a683bf
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-02 14:26:54 +0000

    Added 200 code if everything goes well

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_user(user_id):
         "user_id": user_id,
     }
 
-    return render_template('frontend/load_url.html', user_data=jsonify(user_data))
+    return render_template('frontend/load_url.html', user_data=jsonify(user_data)), 200
     # return jsonify(user_data), 200
     # render_template isn't defined; not anymore oh it seems to work now
 
