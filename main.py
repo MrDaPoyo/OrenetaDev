645e8ce01185a6807c500a3f629b076c95d87898
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-02 14:31:51 +0000

    trying

--- main.py
+++ main.py
@@ -12,5 +12,16 @@ def get_user(user_id):
     # return jsonify(user_data), 200
     # render_template isn't defined; not anymore oh it seems to work now
 
+@app.route("/")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+    }
+
+    return render_template('frontend/index.html'), 200
+    # return jsonify(user_data), 200
+    # render_template isn't defined; not anymore oh it seems to work now
+
+
 if __name__ == "__main__":
     app.run(debug=True)
