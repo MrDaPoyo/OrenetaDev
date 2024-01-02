498c695f6c57c122c34249fe52c969280ff00425
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-02 11:33:57 +0000

    Minor fixes

---
+++ AI/main..py
@@ -0,0 +1,8 @@
+# required modules to make this bizarre creation work properly
+import random 
+import json 
+import pickle 
+import numpy as np 
+import nltk 
+from keras.models import load_model 
+from nltk.stem import WordNetLemmatizer 
